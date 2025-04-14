e3ebb0b5af3d7f6d8e2307ec660f161edc2994d3
PatrickGP11 <patrickgonsalves06@gmail.com>
2025-04-14 18:07:13 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,26 @@
+var frutas = ["Laranja", "Banana"]
+
+/* .push() Adiciona no final */
+frutas.push("Maça")
+
+/* .unshift() Adiciona no começo */
+frutas.unshift("Uva")
+
+ /* Remove o ultimo */
+/* frutas.pop()
+
+/* Remove o primeiro */
+/* frutas.shift() */
+
+/* .splice(1,1) Primeiro valor - item | Segundo valor = qntd*/
+/* frutas.splice(1,1) */
+
+/* const user = prompt("Qual fruta deseja deletar")
+const deleteFruta = frutas.indexOf(user)
+
+frutas.splice(deleteFruta, 1)
+
+console.log(frutas) */
+
+console.log(frutas)
+console.log(frutas.includes("Laranja"))
